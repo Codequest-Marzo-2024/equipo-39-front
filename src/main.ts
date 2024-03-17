18a76d10280ec4cf0eae86fdd7b020566a9e6b32
Joel M. Boada <jmboada30@gmail.com>
2024-03-17 19:11:26 -0400

    Add axios-global utility file and update main.ts and tsconfig.json

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import App from './App.vue';
 import './assets/tailwind.css';
 import router from './router';
 
-createApp(App)
-    .use(createPinia)
-    .use(router)
-    .mount('#app')
+const pinia = createPinia();
+const app = createApp(App);
+
+app.use(pinia);
+app.use(router);
+
+app.mount('#app');
