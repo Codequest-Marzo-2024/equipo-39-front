d24e6332b95bb4aa9dca8e95e04434980084d24c
Joel M. Boada <jmboada30@gmail.com>
2024-03-17 19:11:34 -0400

    Refactor button component props and add default values

--- src/components/button.vue
+++ src/components/button.vue
@@ -1,10 +1,17 @@
 <template>
-    <button type="submit" class="bg-white text-[#2F096E] text-xl font-bold rounded-[5rem] w-[70%] p-2">
-        {{ props.text}}</button>
+    <button :type="type" class="bg-white text-[#2F096E] text-xl font-bold rounded-[5rem] w-[70%] p-2">
+        {{ props.text }}
+    </button>
 </template>
 
 <script setup lang="ts">
-const props = defineProps({
-    text: String,
+interface Props {
+    text: string;
+    type?: 'submit' | 'button';
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    type: 'button',
+    text: 'Label'
 });
 </script>
