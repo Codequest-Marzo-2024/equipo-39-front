45d33f31181eed1746b622327d3204f7d6e63086
miguelFernandez98 <139988212+miguelFernandez98@users.noreply.github.com>
2024-03-17 23:25:41 -0400

    responsive CreateView

--- src/components/input.vue
+++ src/components/input.vue
@@ -1,31 +1,47 @@
 <template>
     <div class="relative mb-5 w-full">
-        <input :type="type"
-            class="block border-2 border-[#FC9D68] bg-[#2F096E] text-white rounded-md w-full p-3 focus:outline-none focus:border-[#FC9D68] peer placeholder-transparent"
-            v-model="value" ref="inputRef" @focus="isFocused = true" @blur="isFocused = false" />
-        <label for="floating_label"
-            :class="{ 'translate-y-0 scale-100 peer-focus:bg-[#2F096E]': value === '' && !isFocused, '-translate-y-[1.45rem] scale-100 peer-focus:bg-[#2F096E]': value !== '' || isFocused }"
-            class="absolute top-3 left-3 text-white px-2 transition-all transform origin-left bg-[#2F096E] pointer-events-none">
-            {{ label }}
-        </label>
+      <input :type="inputType"
+        class="block border-2 border-[#FC9D68] bg-[#2F096E] text-white rounded-md w-full p-3 focus:outline-none focus:border-[#FC9D68] peer placeholder-transparent"
+        v-model="value" ref="inputRef" @focus="handleFocus" @blur="handleBlur" />
+      <label for="floating_label"
+        :class="{ 'translate-y-0 scale-100 peer-focus:bg-[#2F096E]': value === '' && !isFocused, '-translate-y-[1.45rem] scale-100 peer-focus:bg-[#2F096E]': value !== '' || isFocused }"
+        class="absolute top-3 left-3 text-white px-2 transition-all transform origin-left bg-[#2F096E] w-fit pointer-events-none">
+        {{ label }}
+      </label>
     </div>
-</template>
-
-<script setup lang="ts">
-import { ref, watchEffect, defineProps, defineEmits } from 'vue';
-
-const props = defineProps({
+  </template>
+  
+  <script setup lang="ts">
+  import { ref, watchEffect, defineProps, defineEmits } from 'vue';
+  
+  const props = defineProps({
     type: String,
     label: String,
     modelValue: String,
-});
-
-const emit = defineEmits(['update:modelValue']);
-
-const value = ref(props.modelValue);
-const isFocused = ref(false);
-
-watchEffect(() => {
+  });
+  
+  const emit = defineEmits(['update:modelValue']);
+  
+  const value = ref(props.modelValue);
+  const isFocused = ref(false);
+  const inputType = ref('text');
+  
+  const handleFocus = () => {
+    isFocused.value = true;
+    inputType.value = props.type;
+  };
+  
+  const handleBlur = () => {
+    isFocused.value = false;
+    inputType.value = 'text';
+  };
+  
+  watchEffect(() => {
     emit('update:modelValue', value.value);
-});
-</script>
+  });
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
