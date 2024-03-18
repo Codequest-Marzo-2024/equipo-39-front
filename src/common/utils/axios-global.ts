8e1144d1f2dc2983ba9ff8a9dbf53669a2e55e11
Joel M. Boada <jmboada30@gmail.com>
2024-03-17 21:10:03 -0400

    Add useDates composable and update axios-global

--- src/common/utils/axios-global.ts
+++ src/common/utils/axios-global.ts
@@ -4,4 +4,12 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
 });
 
+api.interceptors.request.use((config) => {
+  const jwt = localStorage.getItem('jwt');
+  if (jwt) {
+    config.headers.Authorization = `Bearer ${jwt}`;
+  }
+  return config;
+});
+
 export default api;
